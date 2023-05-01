7ae3dec887c9e24ed63ec426c3e0df223d5cca64
弘山彩 <127706085+cctHiroyama@users.noreply.github.com>
2023-05-01 17:23:48 +0900

    Add files via upload

---
+++ src/main/resources/data.sql
@@ -0,0 +1,26 @@
+INSERT INTO m_member 
+(name, furigana) 
+VALUES 
+('宮島秀幸', 'みやじまひでゆき'),
+('岡野和行',  'おかのかずゆき'),
+('後藤龍彌',  'ごとうたつや'),
+('奥野有希', 'おくのゆき'),
+('白地慧海', 'しらじさとみ'),
+('弘山彩',  'ひろやまあや'),
+('岡田祐可子',  'おかだゆかこ');
+
+INSERT INTO t_member 
+(team_position, birth_place, hobby, status_message) 
+VALUES 
+('', '', '', ''),
+('', '', '', ''),
+('', '', '', ''),
+('', '', '', ''),
+('', '', '', ''),
+('', '', '', ''),
+('', '', '', '');
+
+INSERT INTO diary 
+(title, text) 
+VALUES 
+('テスト', 'テスト');
